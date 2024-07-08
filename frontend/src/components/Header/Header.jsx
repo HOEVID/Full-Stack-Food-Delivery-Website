7f1387adac3716af1a6f45ed9642e1c2cae7a4dc
HOEVID <shreyanshop22@gmail.com>
2024-07-08 11:30:23 +0530

    First Commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className='header-contents'>
+        <h2>Order your favorite for here</h2>
+        <p>Indulge in a mouthwatering array of flavors, from classic favorites to innovative creations. Each dish is a masterpiece, crafted with passion to ignite your senses and delight your palate"</p>
+        <button>View menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header

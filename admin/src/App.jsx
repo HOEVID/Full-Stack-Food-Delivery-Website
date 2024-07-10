28fc4a6c15e1c319d6dcbbbd1f60e9ec3d85bf7a
HOEVID <shreyanshop22@gmail.com>
2024-07-10 13:39:10 +0530

    added routes for pages

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
+import { Route, Routes } from 'react-router-dom'
+import List from './pages/Add/List/List'
+import Orders from './pages/Add/Orders/Orders'
+import Add from './pages/Add/Add'
 
 const App = () => {
   return (
@@ -9,6 +13,11 @@ const App = () => {
       <hr />
       <div className="app-contents">
         <Sidebar/>
+        <Routes>
+          <Route path="/add" element={<Add/>}/>
+          <Route path="/list" element={<List/>}/>
+          <Route path="/orders" element={<Orders/>}/>
+        </Routes>
       </div>
     </div>
   )

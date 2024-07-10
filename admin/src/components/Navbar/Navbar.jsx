d19a3e86135b4adc45bffd471b1800a409945562
HOEVID <shreyanshop22@gmail.com>
2024-07-10 13:31:35 +0530

    navbar & sidebar component completed in admin panel

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import {assets} from '../../assets/assets'
+import logo from '../../assets/logo.png'
+import './Navbar.css'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <img className="logo" src={logo} alt="" />
+        <img  className="profile" src={assets.profile_image} alt="" />
+    </div>
   )
 }
 

ea5994db553727c0a1203ebc28366a55b4bc0624
HOEVID <shreyanshop22@gmail.com>
2024-07-11 12:25:11 +0530

    list css updated and removeFood functionality added in list page on admin panel

--- admin/src/pages/Add/List/List.jsx
+++ admin/src/pages/Add/List/List.jsx
@@ -8,7 +8,6 @@ const List = () => {
   const [list,setList] =useState([]);
   const fetchList =async()=>{
     const response =await axios.get(`${url}/api/food/list`);
-    console.log(response.data);
     if(response.data.success){
       setList(response.data.data);
     }
@@ -16,6 +15,16 @@ else{
   toast.error("Error");
 }
   }
+  const removeFood= async(foodId)=>{
+const response =await axios.post(`${url}/api/food/remove`,{id:foodId})
+ await fetchList();
+ if(response.data.success){
+  toast.success(response.data.message);
+ }
+ else{
+  toast.error("Error");
+ }
+}
   useEffect(()=>{
     fetchList();
   },[])
@@ -37,7 +46,7 @@ else{
     <p>{item.name}</p>
     <p>{item.category}</p>
     <p>{item.price}</p>
-    <p>x</p>
+    <p onClick={()=>removeFood(item._id)} className='cursor'>x</p>
     </div>
  )
 })}

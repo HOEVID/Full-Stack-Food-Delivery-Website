6641ee4f81a626ca5841e6e71dd0947bbaf2769f
HOEVID <shreyanshop22@gmail.com>
2024-07-12 11:27:30 +0530

    login popup signin data in console

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,31 +1,43 @@
 import React, { useState } from 'react'
 import './LoginPopup.css'
 import { assets } from '../../assets/assets'
-const LoginPopup = ({setShowLogin}) => {
-    const[currState,setCurrState]=useState("Login")
+const LoginPopup = ({ setShowLogin }) => {
+  const [currState, setCurrState] = useState("Login")
+  const[data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  })
+  const onChangeHandler = (event => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData(data => ({ ...data, [name]: value }))
+
+  })
+ 
   return (
     <div className='login-popup'>
-        <form action="" className="login-popup-containier">
-            <div className="login-popup-title">
-                <h2>{currState}</h2>
-                <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt=""/>
-            </div>
-            <div className="login-popup-inputs">
-                {currState==="Login"?<></>:<input type= "text" placeholder='Your Name' required/>}
-                
-                <input type= "email" placeholder='Your email' required/>
-                <input type= "password" placeholder='Your password' required/>
-            </div>
-            <button>{currState==="Sign Up"?"Create account":"Login"} </button>
-          <div className="login-popup-condition">
-            <input type="checkbox" />
-            <p>By agreeing , i agree to the terma and conditions</p>
-          </div>
-          {currState==="Login"? <p> Create a new account? <span onClick={()=>setCurrState("Sign Up")}>Click here</span></p>
-          : <p>Already have an account <span onClick={()=>setCurrState("Login")}>Login here</span></p>}
-         
-          
-        </form>
+      <form action="" className="login-popup-containier">
+        <div className="login-popup-title">
+          <h2>{currState}</h2>
+          <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="" />
+        </div>
+        <div className="login-popup-inputs">
+          {currState === "Login" ? <></> : <input name='name' onChange={onChangeHandler} value={data.name} type="text" placeholder='Your Name' required />}
+
+          <input name='email' onChange={onChangeHandler} valuet={data.email} type="email" placeholder='Your email' required />
+          <input name='password' onChange={onChangeHandler} valuet={data.password} type="password" placeholder='Your password' required />
+        </div>
+        <button>{currState === "Sign Up" ? "Create account" : "Login"} </button>
+        <div className="login-popup-condition">
+          <input type="checkbox" />
+          <p>By agreeing , i agree to the terma and conditions</p>
+        </div>
+        {currState === "Login" ? <p> Create a new account? <span onClick={() => setCurrState("Sign Up")}>Click here</span></p>
+          : <p>Already have an account <span onClick={() => setCurrState("Login")}>Login here</span></p>}
+
+
+      </form>
     </div>
   )
 }

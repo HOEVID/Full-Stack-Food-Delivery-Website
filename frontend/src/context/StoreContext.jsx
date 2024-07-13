cc31ab96973fdb94ebe4518c1ae1d16adffaecab
HOEVID <shreyanshop22@gmail.com>
2024-07-13 13:58:53 +0530

    integrated cart add api to frontend

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,12 +7,15 @@ const StoreContextProvider = (props) => {
     const url = "http://localhost:4000";
     const [token, setToken] = useState("");
     const[food_list,setFoodList]=useState([]);
-    const addToCart = (itemId) => {
+    const addToCart = async (itemId) => {
         if (!cartItems[itemId]) {
             setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
         }
         else {
             setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
+        }
+        if(token){ //integrating cart backend api to frontend//
+            await axios.post(url+"/api/cart/add",{itemId},{headers:{token}})
         }
     }
     const removeFromCart = (itemId) => {

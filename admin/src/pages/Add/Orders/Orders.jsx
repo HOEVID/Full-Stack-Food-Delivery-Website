41cc4c6d3ea27bf1b8adae1a46e80dfeb2017115
HOEVID <shreyanshop22@gmail.com>
2024-07-16 21:54:29 +0530

    Integrated order status api to admin panel

--- admin/src/pages/Add/Orders/Orders.jsx
+++ admin/src/pages/Add/Orders/Orders.jsx
@@ -22,7 +22,15 @@ const Orders = ({url}) => {
       toast.error("Error fetching orders")
     }
   }
-
+const statusHandler = async(event,orderId)=>{
+const response =  await axios.post(url+"/api/order/status",{
+  orderId,
+  status:event.target.value
+})
+if(response.data.success){
+  await fetchAllOrders();
+}
+}
   useEffect(()=>{
     fetchAllOrders();
   },[url])
@@ -59,10 +67,10 @@ const Orders = ({url}) => {
                 </div>
                <p>Items:{order.items.length}</p>
                <p>Amount:${order.amount}.00</p>
-               <select>
+               <select onChange={(event)=>statusHandler(event,order._id)} value={order.status}>
                 <option value="Food Processing">Food Processing</option>
                 <option value="Out for Delivery">Out for Delivery</option>
-                <option value="Delivered"></option>
+                <option value="Delivered">Delivered</option>
                </select>
             </div>
         ))}
